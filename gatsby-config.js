1ae817aced66f8f32f8b5272a6f21d5be92d015b
Jonathan Poston <jfposton2@gmail.com>
2022-06-27 21:52:17 -0400

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `Forgotten Heritage`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
